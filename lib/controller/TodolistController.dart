cf8115643287e5ea7d04350cf6d4653573f70209
sameea <opsameea@gmail.com>
2024-04-08 20:53:44 +0530

    updation controller added

--- lib/controller/TodolistController.dart
+++ lib/controller/TodolistController.dart
@@ -9,7 +9,7 @@ class TodolistController {
   required String title,
   required bool isDone,
 }) {
-  var task = TodoListModel(title: title, isDone: isDone); // Create a TodoListModel object
+  var task = TodoListModel(title: title, isDone: isDone); 
   mybox.add(task); // Add the object to the box
   todokeys = mybox.keys.toList();
 }
@@ -19,8 +19,15 @@ class TodolistController {
     todokeys = mybox.keys.toList();
   }
 
-  static void deletetodo(var key) {
-    mybox.delete(key);
+  static Future<void> deletetodo(var key) async {
+    await mybox.delete(key);
     todokeys = mybox.keys.toList();
   }
+
+  static updateTodo(var key, TodoListModel todoModel) async {
+    await mybox.put(key, todoModel);
+    getInitKeys();
+    print(todoModel.isDone);
+  }
+
 }
